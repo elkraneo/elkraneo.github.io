f53f28fd01d1138e4d690db0cc44af8421f08673
Cristian Díaz <cristian@210x7.io>
2023-11-24 22:06:11 +0100

    wip

---
+++ assets/js/resize.js
@@ -0,0 +1,13 @@
+function resize() {
+  var leSize = $('.glslCanvas').parent().width();
+  $('.glslCanvas').width(leSize);
+  $('.glslCanvas').height(leSize);
+}
+
+$(window).resize(function() {
+  resize();
+});
+
+$(window).bind("load", function() {
+	resize();
+});
